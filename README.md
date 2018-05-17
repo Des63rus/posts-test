c0dd1ecdf26e1dd1c0b2d1ff75517d40198038a0
Andrei Kolesnikov <andreik@il.ibm.com>
2018-05-17 14:28:47 +0300

    readme and filter

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# posts-test" 
+# Project Title
+
+Small test project - a web page that contains a comment feed with the ability to insert new comments and filter the existing comments.
+This project working with cloud hosted mongodb instance
+
+### Installing
+
+
+```
+1. Clone repository
+2. In root folder run 'npm install' command
+3. Go to 'client' folder and run 'npm install' command
+4. Go back to root folder and run 'npm run dev'
+5. The application will be running on http://localhost:3000/
+```
+
--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
+import _filter from 'lodash/filter';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 import Post from './Post';
-
 class PostsList extends Component {
+
+  constructor(params) {
+    super(params);
+    this.state = {
+      filter: ''
+    }
+    this.onFilterChange = this.onFilterChange.bind(this);
+  }
+
   componentDidMount() {
     this.props.fetchPosts();
   }
 
   renderPosts() {
-    return this.props.posts.reverse().map(post => {
+    let posts = this.props.posts.reverse();
+    const filter = this.state.filter;
+    if (filter !== undefined && filter !== '') {
+      posts = _filter(posts, post => this.searchWordIndexInValue(post.email, filter) !== -1);
+
+    }
+    return posts.map(post => {
       return (
-         <Post post={post}/>
+        <Post post={post} />
       );
     });
+  }
+
+  onFilterChange(event) {
+    this.setState({
+      filter: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
+        <input placeholder='Filter by email' onChange={this.onFilterChange} />
         {this.renderPosts()}
       </div>
     );
+  }
+
+  searchWordIndexInValue(value, searchWord) {
+    if (value !== null && value !== undefined && searchWord !== null && searchWord !== undefined) {
+      return value.toLowerCase().indexOf(searchWord.toLowerCase());
+    }
+    return -1;
   }
 }
 
